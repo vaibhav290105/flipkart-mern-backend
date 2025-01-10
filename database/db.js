1cfe8a936299bba473665cffd96298a92f33e9d1
vaibhav290105 <vaibhavgupta290105@gmail.com>
2025-01-10 16:54:21 +0530

    third commit

--- database/db.js
+++ database/db.js
@@ -1,16 +1,27 @@
 import mongoose from 'mongoose';
+import mongooseSequence from 'mongoose-sequence'; // Import mongoose-sequence
 
 const Connection = async (username, password) => {
-    // const URL = `mongodb://${username}:${password}@ecommerceweb-shard-00-00.fdvft.mongodb.net:27017,ecommerceweb-shard-00-01.fdvft.mongodb.net:27017,ecommerceweb-shard-00-02.fdvft.mongodb.net:27017/ECOMMERCE?ssl=true&replicaSet=atlas-8a6bhp-shard-0&authSource=admin&retryWrites=true&w=majority`;
-    // const URL = `mongodb://${username}:${password}@ecommerce-shard-00-00.fdvft.mongodb.net:27017,ecommerce-shard-00-01.fdvft.mongodb.net:27017,ecommerce-shard-00-02.fdvft.mongodb.net:27017/ECOMMERCE?ssl=true&replicaSet=atlas-ilaj5d-shard-0&authSource=admin&retryWrites=true&w=majority`;
-    const URL = `mongodb://${username}:${password}@cluster0-shard-00-00.hxfjz.mongodb.net:27017,cluster0-shard-00-01.hxfjz.mongodb.net:27017,cluster0-shard-00-02.hxfjz.mongodb.net:27017/?ssl=true&replicaSet=atlas-5a0t1w-shard-0&authSource=admin&retryWrites=true&w=majority&appName=Cluster0`
-    try {
-        await mongoose.connect(URL, { useUnifiedTopology: true, useNewUrlParser: true, useFindAndModify: false });
-        console.log('Database Connected Succesfully');
-    } catch(error) {
-        console.log('Error: ', error.message);
-    }
+  const URL = `mongodb://${username}:${password}@cluster0-shard-00-00.hxfjz.mongodb.net:27017,cluster0-shard-00-01.hxfjz.mongodb.net:27017,cluster0-shard-00-02.hxfjz.mongodb.net:27017/?ssl=true&replicaSet=atlas-5a0t1w-shard-0&authSource=admin&retryWrites=true&w=majority&appName=Cluster0`;
 
+  try {
+    // Connect to MongoDB
+    await mongoose.connect(URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true, // Ensure indexes are created
+      useFindAndModify: false, // Avoid deprecated findAndModify
+      authMechanism: 'SCRAM-SHA-1', // Explicitly define the auth mechanism
+    });
+
+    // Apply mongoose-sequence plugin to Mongoose instance
+    mongoose.plugin(mongooseSequence);
+
+    console.log('Database Connected Successfully');
+  } catch (error) {
+    console.error('Database Connection Failed:', error.message);
+    process.exit(1); // Exit process with failure code
+  }
 };
 
 export default Connection;
